038909981947859a06913a249e0fafc7e9ba7ed5
ORybka <olha.rybchynska@gmail.com>
2022-12-17 17:54:47 +0200

    add basic Header for desktop

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
+import {Link, useLocation, useNavigate} from "react-router-dom";
+import {HeaderLink} from "../../interfaces";
 import './header.scss';
 
 function Header() {
+    const {pathname} = useLocation();
+    const navigate = useNavigate();
+
+    const headerLinksArr: Array<HeaderLink> = [
+        {
+            text: 'Головна'
+        },
+        {
+            hash: '#conditions',
+            text: 'Умови'
+        },
+        {
+            hash: '#faq',
+            text: 'FAQ'
+        }
+    ];
+
     return (
-        <div className='header-container'>
-            <div className="main-container">
-                <h4 className='title'>Header</h4>
+        <header className='header-container'>
+            <div className="header-inner-container main-container">
+                <div className="logo-block">
+                    <Link to='/'>
+                        <img src={`${process.env.PUBLIC_URL}/assets/images/logo.svg`} alt="logo"/>
+                    </Link>
+                </div>
+                <nav className="navbar-block">
+                    <ul>
+                        {headerLinksArr.map((link, i) => {
+                            return (<li key={i}>
+                                <Link to={{pathname: `${pathname === '/' ? '' : '/'}`, hash: `${link.hash ? link.hash : ''}`}}>{link.text}</Link>
+                            </li>)
+                        })}
+                        <li className='header-btn' onClick={() => navigate('/cabinet/')}>Особистий кабінет</li>
+                    </ul>
+                </nav>
             </div>
-        </div>
+        </header>
     );
 }
 
